443fd978633bfcf6b34ea04e3cc1950df7be0011
Leon <526247082@qq.com>
2017-12-09 22:00:34 +0800

    Add ConstraintLayout.

--- app/src/main/java/com/itheima/screenadaptdemo/MainActivity.java
+++ app/src/main/java/com/itheima/screenadaptdemo/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.percent_layout);
     }
 }
